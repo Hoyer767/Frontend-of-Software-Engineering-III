4dca9c8811eb25a74d8baa8f11d53710b84f1ece
pqhpqhpqh <221830040@smail.nju.edu.cn>
2025-04-16 17:18:40 +0800

    init

---
+++ src/component/TaskItem.vue
@@ -0,0 +1,78 @@
+<script setup lang="ts">
+import { computed, onMounted, ref } from "vue";
+
+const url = computed(() => {
+  const randomNum = Math.floor(Math.random() * 7) + 1;
+  return `src/assets/TaskItemPic/pic${randomNum}.jpg`;
+});
+
+const rotation = ref(0);
+
+onMounted(() => {
+  rotation.value = Math.floor(Math.random() * 11) - 5;
+});
+</script>
+
+<template>
+  <div class="task-item">
+    <el-card
+        style="width: 360px; position: relative"
+        shadow="hover"
+        class="card"
+        :style="{ transform: `rotate(${rotation}deg)` }"
+    >
+      <div class="status-tag" :class="`status-completed`">
+        completed
+      </div>
+      <el-text class="title"> Yummy hamburger </el-text>
+      <img
+          :src="url"
+          style="width: 100%"
+          class="pic"
+      />
+      <el-text class="classification"> Rag评估 </el-text>
+      <delete theme="outline" size="20" class="delete" fill="#333" style="position: absolute; right: 20px; bottom: 20px;"/>
+    </el-card>
+  </div>
+</template>
+
+<style scoped>
+.status-tag {
+  position: absolute;
+  top: 12px;
+  right: 12px;
+  padding: 4px 8px;
+  border-radius: 12px;
+  font-size: 12px;
+  font-weight: 500;
+  color: white;
+}
+
+.status-pending { background: #FF9800; }
+.status-processing { background: #2196F3; }
+.status-completed { background: #4CAF50; }
+.status-failed { background: #F44336; }
+
+.card {
+  border-radius: 10px;
+  transition: transform 0.3s ease;
+  transform-origin: center center;
+}
+
+.title {
+  font-weight: bold;
+  font-size: 25px;
+}
+
+.pic {
+  border-radius: 10px;
+}
+
+.classification {
+  font-size: 14px;
+}
+
+.delete {
+
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import router from "@/router/index.js";
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css';
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import * as IconPark from '@icon-park/vue-next';
 
 // 设置全局axios默认值
 axios.defaults.baseURL = 'http://localhost:8080'
@@ -17,6 +18,10 @@ app.use(router)
     .use(ElementPlus)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
+}
+
+for (const [key, component] of Object.entries(IconPark)) {
+    app.component(key, component);
 }
 
 app.mount('#app')
